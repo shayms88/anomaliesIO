fc9171e23ba0754c6aae5f11d4b197e4fbc95f4f
shay.misgav <shay.misgav@fiverr.com>
2020-07-21 10:20:23 +0300

    s

--- anomlaiesIO/anomaliesFinder/models.py
+++ anomlaiesIO/anomaliesFinder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(blank=True)
+    address = models.CharField(max_length=50)
+    profile = models.TextField()
+    def __str__(self):
+        return self.name
--- anomlaiesIO/anomaliesFinder/views.py
+++ anomlaiesIO/anomaliesFinder/views.py
@@ -1,7 +1,22 @@
+import csv, io
+
 from django.shortcuts import render
+from django.contrib import messages
+
+
 
 def anomalies_finder_main(request):
+    if request.method == 'POST':
+        file = request.FILES['file']
+        if not file.name.endswith('.csv'):
+            messages.error(request, 'This is not a CSV file.')
+
+        data = file.read().decode('UTF-8')
+        io_string = io.StringIO(data)
+        # Get file column names
+        for row in csv.reader(io_string, delimiter=','):
+            print(row)
+
+
     context = {}
-    return render(request,
-                  'anomaliesFinder/index.html',
-                  context=context)
+    return render(request, 'index.html', context=context)
